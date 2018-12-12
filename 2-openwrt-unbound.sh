da8e362e96242a4a5031d9eca44337c35eb4f669
Ivan Vari <ivan.adam.vari@ivanvari.com>
2018-12-12 22:27:25 +0100

    update: 2-openwrt-unbound.sh, added port parameter, cosmetic changes

--- 2-openwrt-unbound.sh
+++ 2-openwrt-unbound.sh
@@ -1,21 +1,23 @@
-# https://blog.cloudflare.com/dns-over-tls-for-openwrt/
+# https://blog.cloudflare.com/dns-over-tls-for-openwrt
 # https://openwrt.org/docs/guide-user/services/dns/unbound
 # https://github.com/openwrt/packages/blob/master/net/unbound/files/README.md
 
 NETWORKID=lan
 FIREWALLZONE=${NETWORKID}
-UNBOUND_EXT="/etc/unbound/unbound_ext.conf"
+UNBOUND_CONF="/etc/unbound/unbound_ext.conf"
+UNBOUND_PORT=5353
 
 opkg update
 
 test -n "$(opkg list-installed unbound)" || opkg install unbound
 test -n "$(opkg list-installed luci-app-unbound)" || opkg install luci-app-unbound
 
-# remove all config except headers
-cp ${UNBOUND_EXT} ${UNBOUND_EXT}.old
-sed '/^[a-z ].*/d' ${UNBOUND_EXT}.old > ${UNBOUND_EXT}
+# remove all config except default headers
+cp ${UNBOUND_CONF} ${UNBOUND_CONF}.old
+sed '/^[a-z ].*/d' ${UNBOUND_CONF}.old > ${UNBOUND_CONF}
 
-cat >> ${UNBOUND_EXT} << EOF
+# append CF config
+cat >> ${UNBOUND_CONF} << EOF
 forward-zone:
   name: "."
   forward-addr: 1.1.1.1@853
@@ -39,7 +41,7 @@ uci batch <<EOF
   set unbound.@unbound[0].domain_type='refuse'
   set unbound.@unbound[0].edns_size='1280'
   set unbound.@unbound[0].hide_binddata='1'
-  set unbound.@unbound[0].listen_port='5353'
+  set unbound.@unbound[0].listen_port=${UNBOUND_PORT}
   set unbound.@unbound[0].localservice='1'
   set unbound.@unbound[0].manual_conf='0'
   set unbound.@unbound[0].protocol='mixed'
@@ -57,14 +59,14 @@ uci batch <<EOF
   delete firewall.${FIREWALLZONE}_unbound
 
   set firewall.${FIREWALLZONE}_unbound=rule
-  set firewall.${FIREWALLZONE}_unbound.name='Reject-Unbound-DNS'
+  set firewall.${FIREWALLZONE}_unbound.name='Reject-${FIREWALLZONE}-Unbound-DNS'
   set firewall.${FIREWALLZONE}_unbound.proto='tcpudp'
   set firewall.${FIREWALLZONE}_unbound.src=${FIREWALLZONE}
-  set firewall.${FIREWALLZONE}_unbound.dest_port='5353'
+  set firewall.${FIREWALLZONE}_unbound.dest_port='${UNBOUND_PORT}'
   set firewall.${FIREWALLZONE}_unbound.target='REJECT'
 
-  add_list dhcp.@dnsmasq[0].server='127.0.0.1#5353'
-  add_list dhcp.@dnsmasq[0].server='::1#5353'
+  add_list dhcp.@dnsmasq[0].server='127.0.0.1#${UNBOUND_PORT}'
+  add_list dhcp.@dnsmasq[0].server='::1#${UNBOUND_PORT}'
 EOF
 
 uci commit && cd /etc/init.d && ./unbound enable && ./unbound restart && ./dnsmasq enable && ./dnsmasq restart && ./firewall reload
