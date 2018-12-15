2e4c6ea320b0751e6e6748fddac49fe60b158f54
Ivan Vari <ivan.adam.vari@ivanvari.com>
2018-12-15 16:55:55 +0100

    new: 0-openwrt-setup.sh, moved setup to position zero, removed pointless changes (default anyway)

---
+++ 0-openwrt-setup.sh
@@ -0,0 +1,56 @@
+# https://openwrt.org/docs/guide-user/base-system/start
+# https://openwrt.org/docs/guide-user/network/start
+# https://openwrt.org/docs/guide-user/firewall/start
+# https://openwrt.org/docs/guide-user/security/lede_security
+
+HOSTNAME=
+ZONENAME=
+TIMEZONE=
+SUBNET=
+MASK=
+
+opkg update
+
+test -n "$(opkg list-installed luci-ssl)" || opkg install luci-ssl
+
+uci batch <<EOF
+  set system.@system[0].zonename=${ZONENAME}
+  set system.@system[0].timezone=${TIMEZONE}
+  set system.@system[0].hostname=${HOSTNAME}
+
+  set network.lan.proto='static'
+  set network.lan.ipaddr=${SUBNET}
+  set network.lan.netmask=${MASK}
+
+  set network.wan.proto='dhcp'
+
+  delete uhttpd.main.listen_http
+
+  set firewall.@defaults[0].custom_chains='1'
+  set firewall.@defaults[0].drop_invalid='1'
+  set firewall.@defaults[0].syn_flood='1'
+  set firewall.@defaults[0].synflood_burst='50'
+  set firewall.@defaults[0].synflood_protect='1'
+  set firewall.@defaults[0].tcp_ecn='1'
+  set firewall.@defaults[0].tcp_syncookies='1'
+  set firewall.@defaults[0].tcp_window_scaling='1'
+  set firewall.@defaults[0].disable_ipv6='0'
+
+  set dropbear.@dropbear[0].Interface='lan'
+  set dropbear.@dropbear[0].BannerFile='/etc/banner'
+
+  set dhcp.@dnsmasq[0].localservice='1'
+  set dhcp.@dnsmasq[0].port='53'
+  set dhcp.@dnsmasq[0].cachesize='1000'
+  set dhcp.@dnsmasq[0].dnsforwardmax='1000'
+  set dhcp.@dnsmasq[0].nohosts='1'
+  set dhcp.@dnsmasq[0].noresolv='1'
+EOF
+
+uci commit && cd /etc/init.d && \
+./uhttpd disable && \
+./system reload && \
+./network restart && \
+./sysntpd reload && \
+./dnsmasq enable && ./dnsmasq restart && \
+./firewall reload
